eb030de9cfc6ad6f0c541b942e7260fc9228dca0
TonyAhn98 <ash981222@gmail.com>
2025-09-09 19:46:20 +0900

    Upload

--- pun2.c
+++ pun2.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
-
+/*
+- function: main
+    - input type: void
+    - output type: int
+    - statements:
+        - float x: type float , variable x
+        - scanf(): input function 
+        - pruntf(): output function
+*/
 int main(void)
 {
-    float x;
-    scanf("%f", &x);
+    float x; 
+    scanf("%f", &x); 
     printf("x = %f", x);
     return 0;
 }
