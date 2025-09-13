7d68d08d23a6fe840704211c963cfeb7ef8a3456
TonyAhn98 <ash981222@gmail.com>
2025-09-13 11:02:35 +0900

    Upload

---
+++ 3rd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5, first_sum, second_sum, total;
+
+    printf ("Enter the first (single) digit:");
+    scanf ("%2d", &d);
+    printf ("%d", d);
+    return 0;
+}
