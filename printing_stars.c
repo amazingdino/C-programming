84913c6f19603e5522bb68c5a60ea92209958f3d
TonyAhn98 <ash981222@gmail.com>
2025-09-20 11:34:54 +0900

    Upload

--- printing_stars.c
+++ printing_stars.c
@@ -7,10 +7,10 @@ int main(void)
     int i, j;
 
     for(int i=0;i<5;i++){ // for every i = 0 and j is less than 5, add 1 value to i after next code
-    for(int j=0;j<=i;j++){ // and for every j = 0 and j is less than or equal to i add 1 value to j after next code
-	printf("*");
+        for(int j=0;j<=i;j++){ // and for every j = 0 and j is less than or equal to i add 1 value to j after next code
+	        printf("*"); 
     }
-    printf("\n");
+        printf("\n");
 }
 
 return 0;
