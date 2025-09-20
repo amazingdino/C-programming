46ee72c03976715b0abcd5c8a20dd56147d1e9a6
TonyAhn98 <ash981222@gmail.com>
2025-09-20 12:37:16 +0900

    Upload

---
+++ star_pyramid.c
@@ -0,0 +1,27 @@
+// still curious how the variable "num" has been assigned by the value of 1 without giving him a value
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, k, num = 1;
+
+    for(i=0; i<5; i++)
+    {
+        for(j=4; j>i; j--)
+        {
+            printf(" ");
+        }
+        //printf("%d\n",num); 
+        for(k=0; k<num; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        
+        num +=2;
+
+    }
+
+    return 0;
+}
