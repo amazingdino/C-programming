3286b9fcbacf33c396b74f5e9dca3d52a23a562b
TonyAhn98 <ash981222@gmail.com>
2025-09-24 22:22:19 +0900

    Upload

--- 12th.c
+++ 12th.c
@@ -1,3 +1,8 @@
+/* This is the basic introduction of the operators (Prefix/postfix)
+* Prefix is where you add or minus 1 the variable's value before the code runs
+* Postfix is where you add or minus 1 the variable's value after the code runs
+*/
+
 #include <stdio.h>
 
 int main(void) 
