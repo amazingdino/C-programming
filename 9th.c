1d510b6f9526145fb23e812db88ceafc8a37e099
TonyAhn98 <ash981222@gmail.com>
2025-09-24 20:59:11 +0900

    Upload

--- 9th.c
+++ 9th.c
@@ -1,3 +1,10 @@
+/*
+* This code is to know what to place for the variables when using printf and scanf
+* PRINTF = only have to write the variable
+* SCANF = you have to write & in front.
+* this is to tell the scanf where the variable is located at.
+*/
+
 #include <stdio.h>
 
 int main(void)
@@ -7,6 +14,6 @@ int main(void)
 
     scanf ("%d%d%f%f", &i, &j, &x, &y);
     printf("%d %d %f %f", i, j, x, y);
-    
+
     return 0;
 }
