4a233c20e212861d1c2e782acf7d7bca59ab8129
TonyAhn98 <ash981222@gmail.com>
2025-09-24 18:03:34 +0900

    Upload

--- 5th.c
+++ 5th.c
@@ -1,3 +1,11 @@
+/* 
+* how to use the directives efficiently.
+* In this case, we know the freezing point is a constant value which is 32.0f. 
+* Also the scale factor is a constant equation transforming celsius to fahrenheit.
+* When we know the value is never going to change or is constant until the end of the code
+* We use #define so that we don't have to write down the numbers all the time.
+*/
+
 #include <stdio.h>
 #define FREEZING_PT 32.0f
 #define  SCALE_FACTOR (5.0f / 9.0f)
