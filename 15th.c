1b775fc1a78a2ada714ac5dcde8c90195cdf4e82
TonyAhn98 <ash981222@gmail.com>
2025-09-26 03:01:54 +0900

    Upload

--- 15th.c
+++ 15th.c
@@ -1,3 +1,9 @@
+/*
+* This is the first time trying if statement
+* It will print Ture since 3 and 3 is equal to each other which makes the condition true
+* if you put !=, it will not print since 3 and 3 DOES EQUAL each other.
+*/
+
 #include <stdio.h>
 
 int main(void)
