a2356be23ce8acd83e2d5bc865b86430d3bc9175
TonyAhn98 <ash981222@gmail.com>
2025-09-26 21:13:37 +0900

    Upload

--- 16th.c
+++ 16th.c
@@ -1,7 +1,7 @@
 /*
-*
-*
-*
+* This code is the application of if statement and using true or false statement to printf what we want
+* When we input a number, it meets the condition if (a>b)
+* Input a as higher value, it's true so you get first if, else prints else.
 */
 
 #include <stdio.h>
