2ebbf5132b6bbe3133b8a187979adc92f601a0c1
TonyAhn98 <ash981222@gmail.com>
2025-09-27 00:16:03 +0900

    Upload

--- inverse_pyramid.c
+++ inverse_pyramid.c
@@ -2,6 +2,8 @@
 * This is how to build an inverse pyramid by using for statement.
 * the i variable makes an empty normal pyramid 
 * the j variable makes next line 
+* the k variable makes the inverse pyramid
+* the l variable makes the inverse pyramid print less by 2 (made it start at 9)
 */
 
 #include <stdio.h>
