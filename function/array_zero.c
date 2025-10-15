3d13755118acd9c0b366299066148fc9fa357507
TonyAhn98 <ash981222@gmail.com>
2025-10-15 13:01:36 +0900

    upload

--- function/array_zero.c
+++ function/array_zero.c
@@ -1,14 +1,14 @@
 // Make the code work 2D array with using function
 #include <stdio.h>
 
-int sum_two_d_array(int a[][2], int n)
+int sum_two_d_array(int r, int c, int a[r][c])
 {
     int i, j, sum;
     sum = 0;
 
-    for(i = 0; i < n; i++)
+    for(i = 0; i < r; i++)
     {
-        for (j = 0; j < 2; j++)
+        for (j = 0; j < c; j++)
         {
             sum += a[i][j];
         }
@@ -20,8 +20,8 @@ int main (void)
 { 
     int a[3][2] = {1,2,3,4,5,6};
     int rows = sizeof(a) / sizeof (a[0]); // sizeof (a) = 6 / sizeof (a[0]) = 3 
-    // int col = sizeof(a) / sizeof (a[][0]);
-    int result = sum_two_d_array(a, rows); // (, 3)
+    int cols = sizeof(a[0]) / sizeof (a[0][0]); // sizeof (a) = 6 / sizeof (a[0][0] = 2)
+    int result = sum_two_d_array(rows, cols, a); // (, 3)
 
     printf("%d", result);
 
