1063c30634e98d6224da6eca5fbe2a886a04076f
TonyAhn98 <ash981222@gmail.com>
2025-11-08 02:29:59 +0900

    Upload

---
+++ Ch.10_variable/variable_i.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int i;
+
+void print_one_row(void)
+{
+    for (i=1; i<=10; i++)
+    {
+        printf("*");
+    }
+}
+
+void print_all_rows(void)
+{
+    for(i=1; i<=10; i++)
+    {
+        print_one_row();
+        printf("\n");
+    }
+}
